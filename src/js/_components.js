d6c98e08f761c67a40177631ad59f1faced9c37f
NataliHTML <nataly07081992@gmail.com>
2023-01-04 17:56:14 +0300

    finish

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,7 @@
-console.log('components');
+import "./components/smooth-scroll";
+import "./components/tooltip";
+import "./components/modals";
+import "./components/slider";
+import "./components/validation";
+import "./components/tabs";
+import "./components/custom-scroll";
